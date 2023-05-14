de0436bc4bc69ed16d8f4105b2686e08fbaec814
Rayan Hantoul <ryan-055@outlook.com>
2023-05-14 11:44:34 +0300

    Update: Solved proplem

--- Solved.py
+++ Solved.py
@@ -1,37 +1,35 @@
 class BankAccount:
+   
     
-
-    initial_balance = 0
-    
-    def __init__(self,account_holder:str, initial_balance:int=0) -> None:
+    def __init__(self,account_holder:str, initial_balance = 0):
         self.account_holder = account_holder
         self.initial_balance = initial_balance
 
-    def deposit(self, add_amount):
+    def deposit(self, add_amount:int):
         
         self.initial_balance += add_amount
         return f"the new balance is {self.initial_balance}"
     
-    def withdraw(self,withdraw_amount):
+    def withdraw(self,withdraw_amount:int):
 
             if self.initial_balance >= withdraw_amount:
                 
                 self.initial_balance -= withdraw_amount
                 return f"the new balance is {self.initial_balance}"
 
-            else:
-                print("Balance is not allowed!!")
+            raise ValueError("Balance is not allowed!!")
 
     def get_balance(self):
         print(f"current balance is: {self.initial_balance}")
 
     def get_account_holder(self):
         print(f"name of account holder is: {self.account_holder}")
+        
 
-process_info = BankAccount
+process_info = BankAccount(account_holder = str(input("Name of account holder: ")))
 
 try:
-    process_info = BankAccount(account_holder= str(input("Name of account holder: ")), initial_balance= int(input("How much the balance: ")))
+    process_info.initial_balance = int(input("How much the initial balance: "))
 except:
     process_info.initial_balance = 0
 
@@ -42,8 +40,8 @@ except:
 
 try:
     print(process_info.withdraw(int(input("how much you want to withdraw? "))))
-except:
-    pass
+except Exception as e:
+    print(e)
 
 
 print(process_info.get_balance())
